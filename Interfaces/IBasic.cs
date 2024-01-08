49b895a490116455c8a4598f6b5ca34fd2d05a98
sean <sgannonumd@gmail.com>
2024-01-07 19:04:13 -0500

    shutup

---
+++ Interfaces/IBasic.cs
@@ -0,0 +1,4 @@
+public interface IBasicService
+{
+    public Task<string> GetString();
+}
---
+++ Services/BasicService.cs
@@ -0,0 +1,10 @@
+public class BasicService : IBasicService
+{
+    private readonly HttpClient _client;
+    public BasicService(HttpClient client) {
+        _client = client;
+    }
+    public async Task<string> GetString() {
+        return "Okay";
+    }
+}
