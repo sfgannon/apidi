eb467dfbdb821904be614b14fb74c3ecbbc59fe5
sean <sgannonumd@gmail.com>
2024-04-19 17:15:37 -0400

    lil fixes

--- Controllers/AlphaVantage.cs
+++ Controllers/AlphaVantage.cs
@@ -9,16 +9,20 @@ namespace ApiDi.Controllers;
 [Route("api/Controller/[action]")]
 public class AlphaVantageController : ControllerBase
 {
-    private readonly string _apiKey;
+    private readonly string? _apiKey;
     private readonly string _baseUri = "https://www.alphavantage.co/query?function={0}&symbol={1}&interval={2}&apikey={3}";
     private readonly HttpClient _client;
     public AlphaVantageController(HttpClient client, IKey keyService) {
         _client = client;
         _apiKey = keyService.GetAlphaVantage();
     }
+    [HttpGet(Name = "GetTS")]
     public async Task<string> GetTS(string pFunction, string pSymbol, string pInterval) {
         string requestUri = string.Format(_baseUri, pFunction, pSymbol, pInterval, _apiKey);
         using HttpResponseMessage msg = await _client.GetAsync(requestUri);
-        return JsonSerializer.Serialize(msg.Content.ReadAsStringAsync());
+        // Serialize this into a TimeSeries, change return type
+        string output = await msg.Content.ReadAsStringAsync();
+        Console.WriteLine(output);
+        return JsonSerializer.Serialize(output);
     }
 }
