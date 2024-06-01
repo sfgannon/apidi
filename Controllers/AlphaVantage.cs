5d49f32e5f5096ce27ede5cc5be5cbf949c85549
sgannonumd@gmail.com <sgannonumd@gmail.com>
2024-06-01 17:20:35 -0400

    fucking too many computers

--- Controllers/AlphaVantage.cs
+++ Controllers/AlphaVantage.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using ApiDi.Classes;
+using ApiDi.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,9 +15,11 @@ public class AlphaVantageController : ControllerBase
     private readonly string? _apiKey;
     private readonly string _baseUri = "https://www.alphavantage.co/query?function={0}&symbol={1}&interval={2}&apikey={3}";
     private readonly HttpClient _client;
-    public AlphaVantageController(HttpClient client, IKey keyService) {
+    private readonly IAVService _service;
+    public AlphaVantageController(HttpClient client, IKey keyService, IAVService service) {
         _client = client;
         _apiKey = keyService.GetAlphaVantage();
+        _service = service;
     }
     [HttpGet(Name = "GetTS")]
     public async Task<string> GetTS(string pFunction = "TIME_SERIES_INTRADAY", string pSymbol = "QQQ", string pInterval = "5min") {
@@ -30,9 +33,10 @@ public class AlphaVantageController : ControllerBase
 
     [HttpGet(Name="TimeSeries")]
     public async Task<AVTS?> GetTimeSeries(string pFunction = "TIME_SERIES_INTRADAY", string pSymbol = "QQQ", string pInterval = "30min") {
-        using HttpResponseMessage msg = await _client.GetAsync(string.Format(_baseUri, pFunction, pSymbol, pInterval, _apiKey));
-        string responseContent = await msg.Content.ReadAsStringAsync();
-        AVTS? ret = JsonSerializer.Deserialize<AVTS>(responseContent, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true});
-        return ret;
+        // using HttpResponseMessage msg = await _client.GetAsync(string.Format(_baseUri, pFunction, pSymbol, pInterval, _apiKey));
+        // string responseContent = await msg.Content.ReadAsStringAsync();
+        // AVTS? ret = JsonSerializer.Deserialize<AVTS>(responseContent, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true});
+        // return ret;
+        return await _service.GetTS(pFunction, pSymbol, pInterval);
     }   
-}   
+}
