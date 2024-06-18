468b4f437a852aa621ed39b9d32b029ac96149a7
sean <sgannonumd@gmail.com>
2024-06-18 06:03:02 -0400

    parsing intrday works, need to update metadata

--- Classes/Utilities.cs
+++ Classes/Utilities.cs
@@ -8,7 +8,14 @@ namespace ApiDi.Classes
     public class AVDateTimeConverter : JsonConverter<DateTime> {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.ParseExact(reader.GetString()!, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            DateTime test = new DateTime();
+            // Do TryParseExact and give format, if it doesnt align its a single full day
+            if (DateTime.TryParseExact(reader.GetString()!, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out test)) {
+                return test;
+            } else {
+                return DateTime.ParseExact(reader.GetString()!, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            }
+            // return DateTime.ParseExact(reader.GetString()!, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
         }
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
